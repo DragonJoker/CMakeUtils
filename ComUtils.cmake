997d63580a6e258d51b390a1cff9e98fbb8daba4
Sylvain Doremus <dragonjoker59@hotmail.com>
2016-03-25 16:09:27 +0000

    Added a way to register COM dlls

--- ComUtils.cmake
+++ ComUtils.cmake
@@ -9,33 +9,33 @@
 #
 #******************************************************************************
 macro( add_com_interfaces OUTPUT_LIST )    
-    foreach( IN_FILE ${ARGN} )
-        get_filename_component( OUT_FILE ${IN_FILE} NAME_WE )
-        get_filename_component( IN_PATH ${IN_FILE} PATH )
+	foreach( IN_FILE ${ARGN} )
+		get_filename_component( OUT_FILE ${IN_FILE} NAME_WE )
+		get_filename_component( IN_PATH ${IN_FILE} PATH )
 
-        set( OUT_HEADER_NAME ${OUT_FILE}_h.h )
-        set( OUT_HEADER ${CMAKE_CURRENT_BINARY_DIR}/${OUT_HEADER_NAME} )
-        set( OUT_IID_NAME ${OUT_FILE}.c )
-        set( OUT_IID ${CMAKE_CURRENT_BINARY_DIR}/${OUT_IID_NAME} )
-    
-        add_custom_command(
-            OUTPUT ${OUT_HEADER} ${OUT_IID}
-            DEPENDS ${IN_FILE}
-            COMMAND midl /header ${OUT_HEADER_NAME} /iid ${OUT_IID_NAME} ${IN_FILE} /I ${CMAKE_CURRENT_SOURCE_DIR}/Src
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        )
+		set( OUT_HEADER_NAME ${OUT_FILE}_h.h )
+		set( OUT_HEADER ${CMAKE_CURRENT_BINARY_DIR}/${OUT_HEADER_NAME} )
+		set( OUT_IID_NAME ${OUT_FILE}.c )
+		set( OUT_IID ${CMAKE_CURRENT_BINARY_DIR}/${OUT_IID_NAME} )
+	
+		add_custom_command(
+			OUTPUT ${OUT_HEADER} ${OUT_IID}
+			DEPENDS ${IN_FILE}
+			COMMAND midl /header ${OUT_HEADER_NAME} /iid ${OUT_IID_NAME} ${IN_FILE} /I ${CMAKE_CURRENT_SOURCE_DIR}/Src
+			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		)
 
-        set_source_files_properties(
-            ${OUT_HEADER}
-            ${OUT_IID}
-            PROPERTIES
-            GENERATED TRUE
-        )
-    
-        set_source_files_properties( ${IN_FILE} PROPERTIES HEADER_FILE_ONLY TRUE )
+		set_source_files_properties(
+			${OUT_HEADER}
+			${OUT_IID}
+			PROPERTIES
+			GENERATED TRUE
+		)
+	
+		set_source_files_properties( ${IN_FILE} PROPERTIES HEADER_FILE_ONLY TRUE )
 
-        set( ${OUTPUT_LIST} ${${OUTPUT_LIST}} ${OUT_HEADER} ${OUT_IID} )
-    endforeach()
+		set( ${OUTPUT_LIST} ${${OUTPUT_LIST}} ${OUT_HEADER} ${OUT_IID} )
+	endforeach()
 endmacro( add_com_interfaces )
 
 #******************************************************************************
@@ -48,14 +48,40 @@ endmacro( add_com_interfaces )
 #
 #******************************************************************************
 macro( add_interface OBJECT_IID RESRC_LIST RESH_LIST RESOURCE_ID OBJECT_NAME )
-    SET( _OBJECT_IID ${OBJECT_IID} )
-    SET( _OBJECT_NAME ${OBJECT_NAME} )
-    configure_file(
-        ${CMAKE_CURRENT_SOURCE_DIR}/Src/Win32/ComCastor.rgs.in
-        ${CMAKE_CURRENT_BINARY_DIR}/Src/Win32/Com${_OBJECT_NAME}.rgs
-        @ONLY
-    )
-    set( ${RESRC_LIST} "${${RESRC_LIST}}\nIDR_${_OBJECT_NAME}	REGISTRY	\"${CMAKE_CURRENT_BINARY_DIR}/Src/Win32/Com${_OBJECT_NAME}.rgs\"" )
-    set( ${RESH_LIST} "${${RESH_LIST}}\n#define IDR_${_OBJECT_NAME}		${${RESOURCE_ID}}" )
-    math( EXPR ${RESOURCE_ID} "${${RESOURCE_ID}}+1")
+	SET( _OBJECT_IID ${OBJECT_IID} )
+	SET( _OBJECT_NAME ${OBJECT_NAME} )
+	configure_file(
+		${CMAKE_CURRENT_SOURCE_DIR}/Src/Win32/ComCastor.rgs.in
+		${CMAKE_CURRENT_BINARY_DIR}/Src/Win32/Com${_OBJECT_NAME}.rgs
+		@ONLY
+	)
+	set( ${RESRC_LIST} "${${RESRC_LIST}}\nIDR_${_OBJECT_NAME}	REGISTRY	\"${CMAKE_CURRENT_BINARY_DIR}/Src/Win32/Com${_OBJECT_NAME}.rgs\"" )
+	set( ${RESH_LIST} "${${RESH_LIST}}\n#define IDR_${_OBJECT_NAME}		${${RESOURCE_ID}}" )
+	math( EXPR ${RESOURCE_ID} "${${RESOURCE_ID}}+1")
 endmacro( add_interface )
+
+#******************************************************************************
+# register_target
+#
+# Notes:
+#   Adds a post build, and a post install events to register a DLL, using
+#   regsvr32.
+#
+#******************************************************************************
+function( register_target _TARGET_NAME _CONFIGURATION )
+	if ( WIN32 )
+		set( _PATH "${PROJECTS_BINARIES_OUTPUT_DIR}/${_CONFIGURATION}/bin/" )
+		file( TO_NATIVE_PATH ${_PATH} _PATH )
+
+		if ( ${_CONFIGURATION} STREQUAL "Debug" )
+			set( _POSTFIX "d" )
+		endif ()
+
+		add_custom_command(
+			TARGET ${_TARGET_NAME}
+			POST_BUILD
+			COMMAND regsvr32 /s "${_PATH}${_TARGET_NAME}${_POSTFIX}.dll"
+			WORKING_DIRECTORY 
+		)
+	endif ()
+endfunction( register_target )
