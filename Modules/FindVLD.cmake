4ce6d109c03edec2ab5630b59a2ca88908724bad
Sylvain Doremus <dragonjoker59@hotmail.com>
2016-05-26 22:06:08 +0100

    Updated FindVLD.cmake to look for x64 lib

--- Modules/FindVLD.cmake
+++ Modules/FindVLD.cmake
@@ -18,12 +18,12 @@ FIND_PACKAGE( PackageHandleStandardArgs )
 
 if( MSVC )
 	FIND_PATH(VLD_ROOT_DIR include/vld.h 
-	  HINTS
-	  PATH_SUFFIXES include vld "Visual Leak Detector"
-	  PATHS
-	  /usr/local
-	  /usr
-	  C:/ Z:/
+		HINTS
+		PATH_SUFFIXES include vld "Visual Leak Detector"
+		PATHS
+		/usr/local
+		/usr
+		C:/ Z:/
 	)
 
 	FIND_PATH(VLD_INCLUDE_DIR vld.h
@@ -33,19 +33,24 @@ if( MSVC )
 	  ${VLD_ROOT_DIR}
 	)
 
-	FIND_PATH(VLD_LIBRARY_DIR vld.lib
-	  HINTS
-	  PATH_SUFFIXES lib/Win32
-	  PATHS
-	  ${VLD_ROOT_DIR}
-	)
+	if (CMAKE_CL_64 OR CMAKE_GENERATOR MATCHES Win64)
+		FIND_PATH(VLD_LIBRARY_DIR vld.lib
+			HINTS
+				PATH_SUFFIXES lib/Win64
+				PATHS ${VLD_ROOT_DIR}
+		)
+	else ()
+		FIND_PATH(VLD_LIBRARY_DIR vld.lib
+			HINTS
+				PATH_SUFFIXES lib/Win32
+				PATHS ${VLD_ROOT_DIR}
+		)
+	endif ()
 
 	FIND_LIBRARY(VLD_LIBRARY
-	  NAMES vld.lib
-	  HINTS
-	  PATH_SUFFIXES lib/Win32
-	  PATHS
-	  ${VLD_LIBRARY_DIR}
+		NAMES vld.lib
+		PATHS
+			${VLD_LIBRARY_DIR}
 	)
 
 	MARK_AS_ADVANCED( VLD_LIBRARY_DIR VLD_LIBRARY )
