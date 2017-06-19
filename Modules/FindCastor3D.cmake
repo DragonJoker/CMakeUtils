5b59d4fac3a289268f50ac5f1a16cc0180475f60
Sylvain Doremus <dragonjoker59@hotmail.com>
2017-06-19 15:16:47 +0200

    Updated and fixed FindCastor3D.cmake

--- Modules/FindCastor3D.cmake
+++ Modules/FindCastor3D.cmake
@@ -15,7 +15,7 @@
 find_package( PackageHandleStandardArgs )
 
 if ( NOT Castor3D_FIND_COMPONENTS )
-	set( Castor3D_FIND_COMPONENTS CastorUtils GlslWriter Castor3D GuiCommon )
+	set( Castor3D_FIND_COMPONENTS CastorUtils GlslWriter Castor3D GuiCommon CastorTest )
 endif ()
 
 #--------------------------------------------------------------------------------------------------
@@ -42,6 +42,8 @@ endfunction()
 #--------------------------------------------------------------------------------------------------
 function( compute_abi_name ABI_Name ABI_Name_Debug )
 	if ( MSVC14 )
+		set( COMPILER "vc15" )
+	elseif ( MSVC14 )
 		set( COMPILER "vc14" )
 	elseif ( MSVC12 )
 		set( COMPILER "vc12" )
@@ -88,136 +90,140 @@ set( Castor3D_FOUND TRUE )
 set( Castor3D_INCLUDE_DIRS "" CACHE STRING "Castor3D include directories" FORCE )
 set( Castor3D_LIBRARIES "" CACHE STRING "Castor3D libraries" FORCE )
 
-foreach( COMPONENT ${Castor3D_FIND_COMPONENTS} )
-	set( ABI_Name )
-	set( ABI_Name_Debug )
-	if ( ${COMPONENT} STREQUAL GuiCommon )
-		compute_abi_name( ABI_Name ABI_Name_Debug )
-	else ()
+find_path( Castor3D_ROOT_DIR include/Castor3D/Castor3DPrerequisites.hpp
+	HINTS
+	PATH_SUFFIXES
+		Castor3D
+	PATHS
+		/usr/local
+		/usr
+)
+
+if ( Castor3D_ROOT_DIR )
+	foreach( COMPONENT ${Castor3D_FIND_COMPONENTS} )
 		set( ABI_Name )
-		set( ABI_Name_Debug "d" )
-	endif ()
-	find_path( Castor3D_${COMPONENT}_ROOT_DIR include/${COMPONENT}/${COMPONENT}Prerequisites.hpp
-		HINTS
-		PATH_SUFFIXES
-			Castor3D
-		PATHS
-			/usr/local
-			/usr
-	)
-
-	find_path( Castor3D_${COMPONENT}_INCLUDE_DIR ${COMPONENT}Prerequisites.hpp
-		HINTS
-		PATH_SUFFIXES
-			include/${COMPONENT}
-		PATHS
-			${Castor3D_${COMPONENT}_ROOT_DIR}
-			/usr/local/include
-			/usr/include
-	)
-
-	if( MSVC )
-		find_path( Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR ${COMPONENT}${ABI_Name}.lib
-			HINTS
-			PATH_SUFFIXES
-				lib
-			PATHS
-				${Castor3D_${COMPONENT}_ROOT_DIR}
-		)
-		find_path( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR ${COMPONENT}${ABI_Name_Debug}.lib
-			HINTS
-			PATH_SUFFIXES
-				lib/Debug
-			PATHS
-				${Castor3D_${COMPONENT}_ROOT_DIR}
-		)
-
-		find_library( Castor3D_${COMPONENT}_LIBRARY_RELEASE
-			NAMES
-				${COMPONENT}${ABI_Name}.lib
-			HINTS
-			PATHS
-				${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR}
-		)
-		find_library( Castor3D_${COMPONENT}_LIBRARY_DEBUG
-			NAMES
-				${COMPONENT}${ABI_Name_Debug}.lib
-			HINTS
-			PATHS
-				${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR}
-		)
-	else()
-		find_path(Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR lib${COMPONENT}${ABI_Name}.so
-			HINTS
-			PATH_SUFFIXES
-				lib64
-				lib
-			PATHS
-				${Castor3D_${COMPONENT}_ROOT_DIR}
-		)
-		find_path(Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR lib${COMPONENT}${ABI_Name_Debug}.so
-			HINTS
-			PATH_SUFFIXES
-				lib64/Debug
-				lib/Debug
-			PATHS
-				${Castor3D_${COMPONENT}_ROOT_DIR}
-		)
-
-		find_library( Castor3D_${COMPONENT}_LIBRARY_RELEASE
-			NAMES
-				lib${COMPONENT}${ABI_Name}.so
-			HINTS
-			PATHS
-				${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR}
-		)
-
-		find_library(Castor3D_${COMPONENT}_LIBRARY_DEBUG
-			NAMES
-				lib${COMPONENT}${ABI_Name_Debug}.so
-			HINTS
-			PATHS
-				${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR}
-		)
-	endif()
-
-	mark_as_advanced( Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR )
-	mark_as_advanced( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR )
-	mark_as_advanced( Castor3D_${COMPONENT}_LIBRARY_RELEASE )
-	mark_as_advanced( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR )
-	find_package_handle_standard_args( Castor3D_${COMPONENT} DEFAULT_MSG Castor3D_${COMPONENT}_LIBRARY_RELEASE Castor3D_${COMPONENT}_INCLUDE_DIR )
-
-	if ( Castor3D_${COMPONENT}_FOUND )
-		if (MSVC)
-			if ( Castor3D_${COMPONENT}_LIBRARY_DEBUG )
-				set( Castor3D_${COMPONENT}_LIBRARIES optimized ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} debug ${Castor3D_${COMPONENT}_LIBRARY_DEBUG} CACHE STRING "Castor3D ${COMPONENT} library" )
-				set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR} ${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR} )
-			else ()
-				set( Castor3D_${COMPONENT}_LIBRARIES ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} CACHE STRING "Castor3D ${COMPONENT} library" )
-				set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR})
-			endif ()
+		set( ABI_Name_Debug )
+		if ( ( ${COMPONENT} STREQUAL GuiCommon ) OR ( ${COMPONENT} STREQUAL CastorTest ) )
+			compute_abi_name( ABI_Name ABI_Name_Debug )
 		else ()
-			if ( Castor3D_LIBRARY_DEBUG )
-				set( Castor3D_${COMPONENT}_LIBRARIES optimized ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} debug ${Castor3D_${COMPONENT}_LIBRARY_DEBUG} CACHE STRING "Castor3D ${COMPONENT} library" )
-				set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR} ${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR} )
-			else ()
-				set( Castor3D_${COMPONENT}_LIBRARIES ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} CACHE STRING "Castor3D ${COMPONENT} library" )
-				set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR})
-			endif ()
+			set( ABI_Name )
+			set( ABI_Name_Debug "d" )
 		endif ()
-		set( Castor3D_INCLUDE_DIRS
-			${Castor3D_INCLUDE_DIRS}
-			${Castor3D_${COMPONENT}_INCLUDE_DIR}
-			CACHE STRING "Castor3D include directories" FORCE
-		)
-		set( Castor3D_LIBRARIES
-			${Castor3D_LIBRARIES}
-			${Castor3D_${COMPONENT}_LIBRARIES}
-			CACHE STRING "Castor3D libraries" FORCE
-		)
-	endif ()
 
-	if ( Castor3D_FOUND AND NOT Castor3D_${COMPONENT}_FOUND )
-		set( Castor3D_FOUND FALSE )
-	endif ()
-endforeach ()
+		find_path( Castor3D_${COMPONENT}_INCLUDE_DIR ${COMPONENT}Prerequisites.hpp
+			HINTS
+			PATH_SUFFIXES
+				include/${COMPONENT}
+			PATHS
+				${Castor3D_ROOT_DIR}
+				/usr/local/include
+				/usr/include
+		)
+
+		if( MSVC )
+			find_path( Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR ${COMPONENT}${ABI_Name}.lib
+				HINTS
+				PATH_SUFFIXES
+					lib
+				PATHS
+					${Castor3D_ROOT_DIR}
+			)
+			find_path( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR ${COMPONENT}${ABI_Name_Debug}.lib
+				HINTS
+				PATH_SUFFIXES
+					lib/Debug
+				PATHS
+					${Castor3D_ROOT_DIR}
+			)
+
+			find_library( Castor3D_${COMPONENT}_LIBRARY_RELEASE
+				NAMES
+					${COMPONENT}${ABI_Name}.lib
+				HINTS
+				PATHS
+					${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR}
+			)
+			find_library( Castor3D_${COMPONENT}_LIBRARY_DEBUG
+				NAMES
+					${COMPONENT}${ABI_Name_Debug}.lib
+				HINTS
+				PATHS
+					${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR}
+			)
+		else()
+			find_path(Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR lib${COMPONENT}${ABI_Name}.so
+				HINTS
+				PATH_SUFFIXES
+					lib64
+					lib
+				PATHS
+					${Castor3D_${COMPONENT}_ROOT_DIR}
+			)
+			find_path(Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR lib${COMPONENT}${ABI_Name_Debug}.so
+				HINTS
+				PATH_SUFFIXES
+					lib64/Debug
+					lib/Debug
+				PATHS
+					${Castor3D_ROOT_DIR}
+			)
+
+			find_library( Castor3D_${COMPONENT}_LIBRARY_RELEASE
+				NAMES
+					lib${COMPONENT}${ABI_Name}.so
+				HINTS
+				PATHS
+					${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR}
+			)
+
+			find_library(Castor3D_${COMPONENT}_LIBRARY_DEBUG
+				NAMES
+					lib${COMPONENT}${ABI_Name_Debug}.so
+				HINTS
+				PATHS
+					${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR}
+			)
+		endif()
+
+		mark_as_advanced( Castor3D_${COMPONENT}_LIBRARY_RELEASE )
+		find_package_handle_standard_args( Castor3D_${COMPONENT} DEFAULT_MSG Castor3D_${COMPONENT}_LIBRARY_RELEASE Castor3D_${COMPONENT}_INCLUDE_DIR )
+
+		if ( Castor3D_${COMPONENT}_FOUND )
+			if (MSVC)
+				if ( Castor3D_${COMPONENT}_LIBRARY_DEBUG )
+					set( Castor3D_${COMPONENT}_LIBRARIES optimized ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} debug ${Castor3D_${COMPONENT}_LIBRARY_DEBUG} CACHE STRING "Castor3D ${COMPONENT} library" )
+					set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR} ${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR} )
+				else ()
+					set( Castor3D_${COMPONENT}_LIBRARIES ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} CACHE STRING "Castor3D ${COMPONENT} library" )
+					set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR})
+				endif ()
+			else ()
+				if ( Castor3D_LIBRARY_DEBUG )
+					set( Castor3D_${COMPONENT}_LIBRARIES optimized ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} debug ${Castor3D_${COMPONENT}_LIBRARY_DEBUG} CACHE STRING "Castor3D ${COMPONENT} library" )
+					set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR} ${Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR} )
+				else ()
+					set( Castor3D_${COMPONENT}_LIBRARIES ${Castor3D_${COMPONENT}_LIBRARY_RELEASE} CACHE STRING "Castor3D ${COMPONENT} library" )
+					set( Castor3D_${COMPONENT}_LIBRARY_DIRS ${Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR})
+				endif ()
+			endif ()
+			set( Castor3D_INCLUDE_DIRS
+				${Castor3D_INCLUDE_DIRS}
+				${Castor3D_${COMPONENT}_INCLUDE_DIR}
+				CACHE STRING "Castor3D include directories" FORCE
+			)
+			set( Castor3D_LIBRARIES
+				${Castor3D_LIBRARIES}
+				${Castor3D_${COMPONENT}_LIBRARIES}
+				CACHE STRING "Castor3D libraries" FORCE
+			)
+		endif ()
+
+		if ( Castor3D_FOUND AND NOT Castor3D_${COMPONENT}_FOUND )
+			set( Castor3D_FOUND FALSE )
+		endif ()
+
+		unset( Castor3D_${COMPONENT}_LIBRARY_RELEASE_DIR CACHE )
+		unset( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR CACHE )
+		unset( Castor3D_${COMPONENT}_LIBRARY_DEBUG_DIR CACHE )
+	endforeach ()
+endif ()
