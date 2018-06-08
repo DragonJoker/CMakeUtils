fcc6e2743e7473a60293b6fae3809cbd3f63d175
Sylvain Doremus <dragonjoker59@hotmail.com>
2018-06-08 13:57:07 +0200

    First attempt to fix compiler version retrieval.

--- CompilerVersion.cmake
+++ CompilerVersion.cmake
@@ -4,14 +4,23 @@
 #--------------------------------------------------------------------------------------------------
 function( DumpCompilerVersion OUTPUT_VERSION)
 	exec_program( ${CMAKE_CXX_COMPILER}
-        ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion 
-        OUTPUT_VARIABLE COMPILER_VERSION
-    )
+		ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion 
+		OUTPUT_VARIABLE COMPILER_VERSION
+	)
 	string( REGEX 
-        REPLACE
-        "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2"
-        COMPILER_VERSION
-        ${COMPILER_VERSION}
-    )
-	set( ${OUTPUT_VERSION} ${COMPILER_VERSION} PARENT_SCOPE )
+		REPLACE
+		"([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2"
+		COMPILER_VERSION_REPL
+		${COMPILER_VERSION}
+	)
+	if ( ( ${COMPILER_VERSION_REPL} STREQUAL "" )
+		OR ( ${COMPILER_VERSION_REPL} STREQUAL ${COMPILER_VERSION} ) )
+		string( REGEX 
+			REPLACE
+			"([0-9])(\\.[0-9])?(\\.[0-9])?" "\\10"
+			COMPILER_VERSION_REPL
+			${COMPILER_VERSION}
+		)
+	endif ()
+	set( ${OUTPUT_VERSION} ${COMPILER_VERSION_REPL} PARENT_SCOPE )
 endfunction()
