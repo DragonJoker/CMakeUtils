90f629e46f83923a610693fb8e868d80fa577e32
DragonJoker <dragonjoker59@hotmail.com>
2018-10-17 11:54:53 +0200

    Reviewed a bit PCHSupport, to make it less dependent on source tree.

--- PCHSupport.cmake
+++ PCHSupport.cmake
@@ -113,28 +113,21 @@ endmacro( pch_get_compile_command )
 MACRO( add_target_precompiled_header TARGET_NAME PCH_HEADER PCH_SOURCE TARGET_CXX_FLAGS )
 	if ( PROJECTS_USE_PRECOMPILED_HEADERS )
 		set( PCH_CURRENT_TARGET ${TARGET_NAME} )
-
+		get_filename_component( PCH_NAME ${PCH_HEADER} NAME )
+		get_filename_component( PCH_PATH ${PCH_HEADER} PATH )
 		if ( MSVC )
 			foreach ( SRC ${ARGN} )
-		    set_source_files_properties( ${SRC} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} /Yu${PCH_HEADER} /FI${PCH_HEADER}" )
+				set_source_files_properties( ${SRC} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} /Yu${PCH_NAME} /FI${PCH_NAME}" )
 			endforeach ()
-			set_source_files_properties( Src/${PCH_SOURCE} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} /Yc${PCH_HEADER}" )
+			set_source_files_properties( ${PCH_SOURCE} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} /Yc${PCH_NAME}" )
 		else ()
-			get_filename_component( PCH_NAME ${PCH_HEADER} NAME)
-			get_filename_component( PCH_PATH ${PCH_HEADER} PATH)
-			set( PCH_OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/Src/${PCH_NAME}.gch" )
-			get_filename_component( PCH_OUTDIR ${PCH_OUTPUT} PATH )
-
-			FILE( MAKE_DIRECTORY ${PCH_OUTDIR} )
-	    	
+			set( PCH_OUTPUT "${PCH_PATH}/${PCH_NAME}.gch" )
 			pch_get_compile_flags( ${TARGET_CXX_FLAGS} PCH_COMPILE_FLAGS )
-	    	
-			pch_get_compile_command( PCH_COMMAND  ${CMAKE_CURRENT_SOURCE_DIR}/Src/${PCH_NAME} ${PCH_OUTPUT} )
-	    	
-			add_custom_command( OUTPUT ${PCH_OUTPUT} COMMAND ${PCH_COMMAND} IMPLICIT_DEPENDS CXX ${CMAKE_CURRENT_SOURCE_DIR}/Src/${PCH_NAME} )
-	    	
+			pch_get_compile_command( PCH_COMMAND  ${PCH_PATH}/${PCH_NAME} ${PCH_OUTPUT} )
+			add_custom_command( OUTPUT ${PCH_OUTPUT} COMMAND ${PCH_COMMAND} IMPLICIT_DEPENDS CXX ${PCH_PATH}/${PCH_NAME} )
+
 			foreach ( SRC ${ARGN} )
-		    set_source_files_properties( ${SRC} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} -include ${PCH_OUTDIR}/${PCH_HEADER} -Wl,--enable-large-address-aware" )
+				set_source_files_properties( ${SRC} PROPERTIES COMPILE_FLAGS "${TARGET_CXX_FLAGS} -include ${PCH_PATH}/${PCH_NAME} -Wl,--enable-large-address-aware" )
 				set_source_files_properties( ${SRC} PROPERTIES OBJECT_DEPENDS ${PCH_OUTPUT} )
 			endforeach ()
 		endif ()
