f69c14882aa737dd3c17717f358bdb6e2f28b8b9
DragonJoker <dragonjoker59@hotmail.com>
2019-10-09 21:52:56 +0200

    Added removal of "/Zm..." from CMAKE_CXX_FLAGS to CompilerVersion, for msvc builds.

--- CompilerVersion.cmake
+++ CompilerVersion.cmake
@@ -24,3 +24,14 @@ function( DumpCompilerVersion OUTPUT_VERSION)
 	endif ()
 	set( ${OUTPUT_VERSION} ${COMPILER_VERSION_REPL} PARENT_SCOPE )
 endfunction()
+
+#--------------------------------------------------------------------------------------------------
+#	MSVC specifics
+#--------------------------------------------------------------------------------------------------
+if( MSVC )
+	# We remove the existing /Zm flag from CMAKE_CXX_FLAGS
+	string( REGEX REPLACE ".*(/Zm[0-9]+).*" "\\1" ZmFlag ${CMAKE_CXX_FLAGS} )
+	if( (NOT "${ZmFlag}" STREQUAL "") AND (NOT "${CMAKE_CXX_FLAGS}" STREQUAL "${ZmFlag}") )
+		string( REPLACE ${ZmFlag} "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+	endif()
+endif()
